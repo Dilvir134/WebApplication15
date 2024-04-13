c686a985aebc880d3f7208039bbee8541fbe881b
Dilvir134 <dilvir134@gmail.com>
2024-04-13 18:17:56 -0400

    Add project files.

---
+++ Services/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
+using System.Net;
+
+namespace WebApplication15.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string body)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+            var smtpClient = new SmtpClient
+            {
+                Host = emailSettings["SmtpServer"],
+                Port = int.Parse(emailSettings["SmtpPort"]),
+                EnableSsl = true,
+                Credentials = new NetworkCredential(emailSettings["SmtpUserName"], emailSettings["SmtpPassword"])
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("dilvir.singh@georgebrown.ca"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(email);
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
